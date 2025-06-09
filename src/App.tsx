0deb230c1934aea85c38130037e6b89fb7a1e22f
jajuanx <jajuan.burton@live.com>
2025-06-08 21:50:46 -0400

    #3 - use useEffect to test locally.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { disneyService } from "./services/disney-service/disney.service.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	useEffect(() => {
+		const getCharacters = async () => {
+			const characters = await disneyService.getCharacters()
+			console.log(characters);
+		}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		getCharacters()
+
+		const getCharacterById = async (id: string | number) => {
+			const character = await disneyService.getCharacterById(id);
+			console.log(character);
+			
+		}
+
+		getCharacterById('112');
+	}, [])
+	return (
+		<>
+			<h1>Disney Characters</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
