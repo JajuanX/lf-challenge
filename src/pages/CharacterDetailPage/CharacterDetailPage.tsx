fa2e51a228486da24143dbaff25a1d9215ad3c8e
jajuanx <jajuan.burton@live.com>
2025-06-10 00:32:03 -0400

    #9 - set up details page

--- src/pages/CharacterDetailPage/CharacterDetailPage.tsx
+++ src/pages/CharacterDetailPage/CharacterDetailPage.tsx
@@ -1,25 +1,116 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { disneyService } from "../../services/disney-service/disney.service";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import './CharacterDetailPage.scss'
+
+type Character = {
+	_id: string;
+	name: string;
+	imageUrl?: string;
+	films: string[];
+	shortFilms: string[];
+	tvShows: string[];
+	allies: string[];
+	enemies: string[];
+	sourceUrl?: string;
+};
 
 const CharacterDetailPage = () => {
-	const { id } = useParams<{id : string}>()
+	const { id } = useParams<{ id: string }>();
+	const [character, setCharacter] = useState<Character | null>();
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState("");
 
 	useEffect(() => {
-		if(!id) return;
+		if (!id) return;
 
-		const getCharacterById = async () => {
-			const character = await disneyService.getCharacterById(id);
-			console.log(character);
+		const fetchCharacter = async () => {
+			try {
+				setLoading(true);
+				setError("");
+
+				const response = await disneyService.getCharacterById(id);
+				console.log(response.data);
+				
+				setCharacter(response.data);
+			} catch (error) {
+				setError("Failed to load Character");
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
 		};
 
-		getCharacterById();
+		fetchCharacter();
 	}, [id]);
 
+	if (loading) return <p className="detail__loading">Loading...</p>;
+	if (error) return <p className="detail__error">{error}</p>;
+	if (!character) return null;
+
 	return (
-		<>
-			<h1>Character Detail Page</h1>
-		</>
+		<main className="detail">
+			<Link to="/" className="detail__back">
+				Back to List
+			</Link>
+			<section className="detail__card">
+				{character.imageUrl && (
+					<img
+						src={character.imageUrl}
+						alt={`Portrait of ${character.name}`}
+						className="detail__image"
+					/>
+				)}
+
+				<h1 className="detail__name">{character.name}</h1>
+
+				<section className="detail__section">
+					{character.films?.length > 0 && (
+						<article>
+							<h2 className="detail__label">Films</h2>
+							<ul className="detail__list">
+								{character.films?.map((film) => {
+									return <li className="detail__item" key={film}>{film}</li>;
+								})}
+							</ul>
+						</article>
+					)}
+					
+					{character.tvShows?.length > 0 && (
+						<article>
+							<h2 className="detail__label">TV Shows</h2>
+							<ul className="detail__list">
+								{character.tvShows?.map((show) => {
+									return <li className="detail__item" key={show}>{show}</li>;
+								})}
+							</ul>
+						</article>
+					)}
+
+					{character.allies?.length > 0 && (
+						<article>
+							<h2 className="detail__label">Allies</h2>
+							<ul className="detail__list">
+								{character.allies?.map((ally) => {
+									return <li className="detail__item" key={ally}>{ally}</li>;
+								})}
+							</ul>
+						</article>
+					)}
+
+					{character.enemies?.length > 0 && (
+						<article>
+							<h2 className="detail__label">Enemies</h2>
+							<ul className="detail__list">
+								{character.enemies?.map((enemy) => {
+									return <li className="detail__item" key={enemy}>{enemy}</li>;
+								})}
+							</ul>
+						</article>
+					)}
+				</section>
+			</section>
+		</main>
 	);
 };
 
